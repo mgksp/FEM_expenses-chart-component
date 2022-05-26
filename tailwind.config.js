7a9fd2515501ed48e6d476af1255f5469ef9fb36
Prabu <prabu.mgks@gmail.com>
2022-05-26 11:32:36 +0530

    setup complete

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        dmSans: ["DM Sans", "sans-serif"],
+      },
+
+      colors: {
+        softRed: "hsl(10, 79%, 65%)",
+        cyan: "hsl(186, 34%, 60%)",
+
+        darkBrown: "hsl(25, 47%, 15%)",
+        mediumBrown: "hsl(28, 10%, 53%)",
+        cream: "hsl(27, 66%, 92%)",
+        veryPaleOrange: "hsl(33, 100%, 98%)",
+      },
+    },
+  },
+  plugins: [],
+};
