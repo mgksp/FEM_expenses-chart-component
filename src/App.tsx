a23514cba8cacadf9a7536f9b398d866d83ffec6
Prabu <prabu.mgks@gmail.com>
2022-05-27 12:11:03 +0530

    attribution

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,25 @@ function App() {
   return (
     <main className="min-h-screen w-full font-dmSans px-4 grid place-content-center text-15 md:text-lg">
       <ChartCard />
+      <div className="absolute left-0 bottom-2 text-xs font-medium text-darkBrown w-full text-center">
+        Challenge by{" "}
+        <a
+          className="text-sm text-softRed font-bold"
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a
+          className="text-sm text-softRed font-bold"
+          href="https://www.github.com/mgksp"
+          target="_blank"
+        >
+          Prabu
+        </a>
+        .
+      </div>
     </main>
   );
 }
